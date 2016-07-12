5376f762fcb336d72a192c138aa6e2bc955fe4b5
evgeniy.bogatov <bogatovevgeniy@gmail.com>
2016-07-12 23:32:03 +0300

    SECOND COMMIT

--- app/src/main/java/com/example/multiwind/rxtest/BluetoothActivity.java
+++ app/src/main/java/com/example/multiwind/rxtest/BluetoothActivity.java
@@ -3,16 +3,21 @@ package com.example.multiwind.rxtest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.widget.Toast;
 
 import java.util.Set;
 
 public class BluetoothActivity extends Activity {
 
+    private static final int REQUEST_ENABLE_BT = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.blouetooth_layout);
         getBluetoothPermissions();
         initBluetooth();
         getDevicesList();
@@ -21,8 +26,22 @@ public class BluetoothActivity extends Activity {
 
     private void getBluetoothPermissions() {
         BluetoothAdapter defaultAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> bondedDevices = defaultAdapter.getBondedDevices();
+        if (defaultAdapter == null){
+            Toast.makeText(this, "Ooops were is bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (!defaultAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK && requestCode == REQUEST_ENABLE_BT){
+
+        }
     }
 
     private void initBluetooth() {
